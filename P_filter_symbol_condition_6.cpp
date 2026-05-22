02cd7307128ae1538eaa78196a287c6e8753c618
shivsomappa <shiv.somappa@gmail.com>
2026-05-22 10:12:57 +0530

    Update P_filter_symbol_condition_6.cpp

--- P_filter_symbol_condition_6.cpp
+++ P_filter_symbol_condition_6.cpp
@@ -1,30 +1,40 @@
+// =============================================================================
+// P_filter_symbol_condition_6.cpp
+// Symbol-sets group (intersection) filter method.
+//
+// Finds groups of cells across blocks in a band or stack where a set of symbols
+// is confined to a group of blocks.  Any occurrence of those symbols outside the
+// group, within the same band/stack, is invalid and is eliminated.
+// Exposed entry point: filter_symbol_sets_group_method()
+// =============================================================================
+
 #include"filters.h"
 
 using namespace std;
 
 
 
-int find_CTS_sgm(Grid2D& temp_1,Grid2D& temp_2,int list_end,int size,int &c_count,int max_set_size);
-int sort_temp_1_data_sgm(Grid2D& temp_1,int &size,int max_set_size);
-int find_single_count_symbols(Grid2D& temp_1, Grid2D& temp_2, int max_set_size, int size);
+int find_CTS_sgm(Grid2D& tally_buf,Grid2D& work_buf,int list_end,int size,int &c_count,int max_options);
+int sort_temp_1_data_sgm(Grid2D& tally_buf,int &size,int max_options);
+int find_single_count_symbols(Grid2D& tally_buf, Grid2D& work_buf, int max_options, int size);
 
-int filter_symbol_hrz_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int x,int size,int max_set_size, int pid, int stat_data[]);
-int filter_symbol_vrt_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int x,int size,int max_set_size, int pid, int stat_data[]);
-int filter_symbol_blk_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int bx,int by,int blk_size,int size,int max_set_size, int pid, int stat_data[]);
+int filter_symbol_hrz_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int x,int size,int max_options, int pred_id, int stats[]);
+int filter_symbol_vrt_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int x,int size,int max_options, int pred_id, int stats[]);
+int filter_symbol_blk_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int bx,int by,int block_size,int size,int max_options, int pred_id, int stats[]);
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int filter_symbol_sets_group_method(Grid3D& p_OT,  Grid2D& temp_1, Grid2D& temp_2,int size,int blk_size,int max_set_size, int pid, int stat_data[])
+int filter_symbol_sets_group_method(Grid3D& opt_tbl,  Grid2D& tally_buf, Grid2D& work_buf,int size,int block_size,int max_options, int pred_id, int stats[])
 {
 	int i=0,j=0, flag=0;
 
 
 	while(i<size&&flag==0)
 	{
-		flag=filter_symbol_hrz_sgm(p_OT, temp_1,temp_2,i,size,max_set_size, pid, stat_data);
+		flag=filter_symbol_hrz_sgm(opt_tbl, tally_buf,work_buf,i,size,max_options, pred_id, stats);
 		i++;
 	}
 
@@ -33,7 +43,7 @@ int filter_symbol_sets_group_method(Grid3D& p_OT,  Grid2D& temp_1, Grid2D& temp_
 		i = 0;
 		while(i<size&&flag==0)
 		{
-			flag = filter_symbol_vrt_sgm(p_OT, temp_1, temp_2, i, size, max_set_size, pid, stat_data);
+			flag = filter_symbol_vrt_sgm(opt_tbl, tally_buf, work_buf, i, size, max_options, pred_id, stats);
 			i++;
 		}
 	}
@@ -46,10 +56,10 @@ int filter_symbol_sets_group_method(Grid3D& p_OT,  Grid2D& temp_1, Grid2D& temp_
 			j = 0;
 			while(j<size&&flag==0)
 			{
-				flag = filter_symbol_blk_sgm(p_OT, temp_1, temp_2, i, j, blk_size, size, max_set_size, pid, stat_data);
-				j = j + blk_size;
+				flag = filter_symbol_blk_sgm(opt_tbl, tally_buf, work_buf, i, j, block_size, size, max_options, pred_id, stats);
+				j = j + block_size;
 			}
-			i = i + blk_size;
+			i = i + block_size;
 		}
 	}
 
@@ -57,15 +67,15 @@ int filter_symbol_sets_group_method(Grid3D& p_OT,  Grid2D& temp_1, Grid2D& temp_
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int filter_symbol_hrz_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int x,int size,int max_set_size, int pid, int stat_data[])
+int filter_symbol_hrz_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int x,int size,int max_options, int pred_id, int stats[])
 {
 	int i=0,j=0,k=0, flag =0, list_end=0,cc=0,c_count,set_1=0;
 	int x1,y1;
 
 	for(i=0;i<size;i++)
 	{
-		if (p_OT(x,i,0) > 0) {cc++;}
-		if (p_OT(x,i,size + 1) == pid) { flag = 1; }
+		if (opt_tbl(x,i,0) > 0) {cc++;}
+		if (opt_tbl(x,i,size + 1) == pred_id) { flag = 1; }
 	}
 
 	//***********************************************************************
@@ -74,46 +84,46 @@ int filter_symbol_hrz_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int x,int
 		flag = 0;
 		for(j = 0; j < size; j++)
 		{
-			if(p_OT(x,j,0) > 0)
+			if(opt_tbl(x,j,0) > 0)
 			{
 				i = 0;
 				for (k = 0; k < size + 1; k++)
 				{
-					if (p_OT(x,j,k) > 0)
+					if (opt_tbl(x,j,k) > 0)
 					{
-						temp_1(list_end,i) = p_OT(x,j,k); i++;
+						tally_buf(list_end,i) = opt_tbl(x,j,k); i++;
 					}
 				}
-				while (i < max_set_size) { temp_1(list_end,i) = 0; i++; }
-				temp_1(list_end,max_set_size) = x;
-				temp_1(list_end,max_set_size + 1) = j;
+				while (i < max_options) { tally_buf(list_end,i) = 0; i++; }
+				tally_buf(list_end,max_options) = x;
+				tally_buf(list_end,max_options + 1) = j;
 				list_end++;
 			}
 		}
 		//***********************************************************************
-		sort_temp_1_data_sgm(temp_1, list_end, max_set_size);
+		sort_temp_1_data_sgm(tally_buf, list_end, max_options);
 
 		if (list_end > 3)
 		{
-			flag = find_CTS_sgm(temp_1, temp_2, list_end, size, c_count, max_set_size);
+			flag = find_CTS_sgm(tally_buf, work_buf, list_end, size, c_count, max_options);
 
 			if (flag == 1)
 			{
 				flag = 0;
-				if (find_single_count_symbols(temp_1, temp_2, max_set_size, size) == 1)
+				if (find_single_count_symbols(tally_buf, work_buf, max_options, size) == 1)
 				{
 					for (i = c_count; i < list_end; i++)
 					{
-						x1 = temp_1(i,max_set_size);
-						y1 = temp_1(i,max_set_size + 1);
+						x1 = tally_buf(i,max_options);
+						y1 = tally_buf(i,max_options + 1);
 						for (k = 1; k < size + 1; k++)
 						{
-							if (p_OT(x1,y1,k) > 0 && temp_2(k,0) > 0)
+							if (opt_tbl(x1,y1,k) > 0 && work_buf(k,0) > 0)
 							{
-								p_OT(x1,y1,k) = 0;
-								p_OT(x1,y1,0)--;			
-								p_OT(x1,y1,size + 1) = pid;
-								stat_data[8]++;
+								opt_tbl(x1,y1,k) = 0;
+								opt_tbl(x1,y1,0)--;			
+								opt_tbl(x1,y1,size + 1) = pred_id;
+								stats[8]++;
 								flag = 1;
 							}
 						}
@@ -128,7 +138,7 @@ int filter_symbol_hrz_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int x,int
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int filter_symbol_vrt_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int y,int size,int max_set_size, int pid, int stat_data[])
+int filter_symbol_vrt_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int y,int size,int max_options, int pred_id, int stats[])
 {
 	int i=0,j=0,k=0, flag=0, list_end=0,cc=0,c_count,set_1=0;
 	int x1,y1;
@@ -136,8 +146,8 @@ int filter_symbol_vrt_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int y,int
 
 	for(i=0;i<size;i++)
 	{
-		if (p_OT(i,y,0) > 0) { cc++; }
-		if (p_OT(i,y,size + 1) == pid) { flag = 1; }
+		if (opt_tbl(i,y,0) > 0) { cc++; }
+		if (opt_tbl(i,y,size + 1) == pred_id) { flag = 1; }
 	}
 	//***********************************************************************
 	if (cc > 4 && flag==1)
@@ -145,42 +155,42 @@ int filter_symbol_vrt_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int y,int
 		flag = 0;
 		for(i=0;i<size;i++)
 		{
-			if(p_OT(i,y,0)>0)
+			if(opt_tbl(i,y,0)>0)
 			{
 				j = 0;
 				for (k = 0; k < size + 1; k++)
 				{
-					if (p_OT(i,y,k) > 0) { temp_1(list_end,j) = p_OT(i,y,k); j++; }
+					if (opt_tbl(i,y,k) > 0) { tally_buf(list_end,j) = opt_tbl(i,y,k); j++; }
 				}
-				while (j < max_set_size) { temp_1(list_end,j) = 0; j++; }
-				temp_1(list_end,max_set_size) = i;
-				temp_1(list_end,max_set_size + 1) = y;
+				while (j < max_options) { tally_buf(list_end,j) = 0; j++; }
+				tally_buf(list_end,max_options) = i;
+				tally_buf(list_end,max_options + 1) = y;
 				list_end++;
 			}
 		}
 		//***********************************************************************
-		sort_temp_1_data_sgm(temp_1,list_end,max_set_size);
+		sort_temp_1_data_sgm(tally_buf,list_end,max_options);
 
 		if (list_end > 3)
 		{
-			flag = find_CTS_sgm(temp_1, temp_2, list_end, size, c_count, max_set_size);
+			flag = find_CTS_sgm(tally_buf, work_buf, list_end, size, c_count, max_options);
 
 			if (flag == 1)
 			{
 				flag = 0;
-				if (find_single_count_symbols(temp_1, temp_2, max_set_size, size) == 1)
+				if (find_single_count_symbols(tally_buf, work_buf, max_options, size) == 1)
 				{
 					for (i = c_count;i < list_end;i++)
 					{
-						x1 = temp_1(i,max_set_size);y1 = temp_1(i,max_set_size + 1);
+						x1 = tally_buf(i,max_options);y1 = tally_buf(i,max_options + 1);
 						for (k = 1;k < size + 1;k++)
 						{
-							if (p_OT(x1,y1,k) > 0 && temp_2(k,0) > 0)
+							if (opt_tbl(x1,y1,k) > 0 && work_buf(k,0) > 0)
 							{
-								p_OT(x1,y1,k) = 0;
-								p_OT(x1,y1,0)--;		
-								p_OT(x1,y1,size + 1) = pid;
-								stat_data[8]++;
+								opt_tbl(x1,y1,k) = 0;
+								opt_tbl(x1,y1,0)--;		
+								opt_tbl(x1,y1,size + 1) = pred_id;
+								stats[8]++;
 								flag = 1;
 							}
 						}
@@ -195,64 +205,64 @@ int filter_symbol_vrt_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int y,int
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int filter_symbol_blk_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int bx,int by,int blk_size,int size,int max_set_size, int pid, int stat_data[])
+int filter_symbol_blk_sgm(Grid3D& opt_tbl,  Grid2D& tally_buf,Grid2D& work_buf,int bx,int by,int block_size,int size,int max_options, int pred_id, int stats[])
 {
 	int i=0,j=0,k=0, flag=0, list_end=0,cc=0,c_count,set_1=0;
 	int x1, y1;
 
 
-	for(i=bx;i<bx+blk_size;i++)
+	for(i=bx;i<bx+block_size;i++)
 	{
-		for(j=by;j<by+blk_size;j++)
+		for(j=by;j<by+block_size;j++)
 		{
-			if (p_OT(i,j,0) > 0) { cc++; }
-			if (p_OT(i,j,size + 1) == pid) { flag = 1; }
+			if (opt_tbl(i,j,0) > 0) { cc++; }
+			if (opt_tbl(i,j,size + 1) == pred_id) { flag = 1; }
 		}
 	}
 	//***********************************************************************
 	if(cc>4 && flag==1)
 	{
 		flag = 0;
-		for(i=bx;i<bx+blk_size;i++)
+		for(i=bx;i<bx+block_size;i++)
 		{
-			for(j=by;j<by+blk_size;j++)
+			for(j=by;j<by+block_size;j++)
 			{
-				if(p_OT(i,j,0)>0)
+				if(opt_tbl(i,j,0)>0)
 				{
 					cc = 0;
 					for (k = 0; k < size + 1; k++)
 					{
-						if (p_OT(i,j,k) > 0) { temp_1(list_end,cc) = p_OT(i,j,k); cc++; }
+						if (opt_tbl(i,j,k) > 0) { tally_buf(list_end,cc) = opt_tbl(i,j,k); cc++; }
 					}
-					while (cc < max_set_size) { temp_1(list_end,cc) = 0; cc++; }
-					temp_1(list_end,max_set_size) = i;
-					temp_1(list_end,max_set_size + 1) = j;
+					while (cc < max_options) { tally_buf(list_end,cc) = 0; cc++; }
+					tally_buf(list_end,max_options) = i;
+					tally_buf(list_end,max_options + 1) = j;
 					list_end++;
 				}
 			}
 		}
 		//***********************************************************************
-		sort_temp_1_data_sgm(temp_1,list_end,max_set_size);
+		sort_temp_1_data_sgm(tally_buf,list_end,max_options);
 		if (list_end > 3)
 		{
-			flag = find_CTS_sgm(temp_1, temp_2, list_end, size, c_count, max_set_size);
+			flag = find_CTS_sgm(tally_buf, work_buf, list_end, size, c_count, max_options);
 
 			if (flag == 1)
 			{
 				flag = 0;
-				if (find_single_count_symbols(temp_1, temp_2, max_set_size, size) == 1)
+				if (find_single_count_symbols(tally_buf, work_buf, max_options, size) == 1)
 				{
 					for (i = c_count;i < list_end;i++)
 					{
-						x1 = temp_1(i,max_set_size);y1 = temp_1(i,max_set_size + 1);
+						x1 = tally_buf(i,max_options);y1 = tally_buf(i,max_options + 1);
 						for (k = 1;k < size + 1;k++)
 						{
-							if (p_OT(x1,y1,k) > 0 && temp_2(k,0) > 0)
+							if (opt_tbl(x1,y1,k) > 0 && work_buf(k,0) > 0)
 							{
-								p_OT(x1,y1,k) = 0;
-								p_OT(x1,y1,0)--;			
-								p_OT(x1,y1,size + 1) = pid;
-								stat_data[8]++;
+								opt_tbl(x1,y1,k) = 0;
+								opt_tbl(x1,y1,0)--;			
+								opt_tbl(x1,y1,size + 1) = pred_id;
+								stats[8]++;
 								flag = 1;
 							}
 						}
@@ -268,7 +278,7 @@ int filter_symbol_blk_sgm(Grid3D& p_OT,  Grid2D& temp_1,Grid2D& temp_2,int bx,in
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int find_CTS_sgm(Grid2D& temp_1,Grid2D& temp_2,int list_end,int size,int &c_count,int max_set_size)
+int find_CTS_sgm(Grid2D& tally_buf,Grid2D& work_buf,int list_end,int size,int &c_count,int max_options)
 {
 	int s,i,j,k,flag=0,start=1,end=list_end,shift_count=0;
 	int s_count=0,c1,c2;
@@ -276,32 +286,32 @@ int find_CTS_sgm(Grid2D& temp_1,Grid2D& temp_2,int list_end,int size,int &c_coun
 
 	while(flag==0)//end>2&&s_count!=c_count)
 	{
-		s_count=temp_1(0,0);c_count=1;start=1;
-		for(i=0;i<size+1;i++){temp_2(i,0) = 0; }
-		for(i=1;i<max_set_size;i++){if(temp_1(0,i)>0){s=temp_1(0,i);temp_2(s,0) = 1; } }
+		s_count=tally_buf(0,0);c_count=1;start=1;
+		for(i=0;i<size+1;i++){work_buf(i,0) = 0; }
+		for(i=1;i<max_options;i++){if(tally_buf(0,i)>0){s=tally_buf(0,i);work_buf(s,0) = 1; } }
 		//**************************************************
 		while(start<end-1&&flag==0)
 		{
-			c2=max_set_size;
+			c2=max_options;
 			for(i=start;i<list_end;i++)
 			{
 				c1=0;
-				for(k=1;k<max_set_size;k++)
+				for(k=1;k<max_options;k++)
 				{
-					if(temp_1(i,k)>0){s=temp_1(i,k);if(temp_2(s,0) == 0) { c1++; } }
+					if(tally_buf(i,k)>0){s=tally_buf(i,k);if(work_buf(s,0) == 0) { c1++; } }
 				}
 				if(c1<c2){j=i;c2=c1;}
-				else if (c1 == c2 && temp_1(j,0) < temp_1(i,0)) { j = i; c2 = c1; }
+				else if (c1 == c2 && tally_buf(j,0) < tally_buf(i,0)) { j = i; c2 = c1; }
 			}
 
 			//****************************
 			if(c2<2)
 			{
 				c_count++;s_count=s_count+c2;
-				for(k=0;k<max_set_size+3;k++)
+				for(k=0;k<max_options+3;k++)
 				{
-					s=temp_1(j,k);temp_1(j,k)=temp_1(start,k);temp_1(start,k)=s;
-					if(k>0&&k<max_set_size&&s>0){temp_2(s,0)++; }
+					s=tally_buf(j,k);tally_buf(j,k)=tally_buf(start,k);tally_buf(start,k)=s;
+					if(k>0&&k<max_options&&s>0){work_buf(s,0)++; }
 				}
 			}
 			if (c_count == s_count-1 || shift_count > end) { flag = 1; }
@@ -314,11 +324,11 @@ int find_CTS_sgm(Grid2D& temp_1,Grid2D& temp_2,int list_end,int size,int &c_coun
 			k = 0;
 			while (k < c_count)
 			{
-				for (j = 0; j < max_set_size + 3; j++) { temp_1(end,j) = temp_1(0,j); }
+				for (j = 0; j < max_options + 3; j++) { tally_buf(end,j) = tally_buf(0,j); }
 
 				for (i = 0; i < end; i++)
 				{
-					for (j = 0; j < max_set_size + 3; j++) { temp_1(i,j) = temp_1(i + 1,j); }
+					for (j = 0; j < max_options + 3; j++) { tally_buf(i,j) = tally_buf(i + 1,j); }
 				}
 				k++;
 			}
@@ -331,20 +341,20 @@ int find_CTS_sgm(Grid2D& temp_1,Grid2D& temp_2,int list_end,int size,int &c_coun
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int sort_temp_1_data_sgm(Grid2D& temp_1,int &size,int max_set_size)
+int sort_temp_1_data_sgm(Grid2D& tally_buf,int &size,int max_options)
 {
 	int i,j,k,s, cc=size;
 
 	for (i = 0; i < size; i++)
 	{
-		if (temp_1(i,0) == 2)
+		if (tally_buf(i,0) == 2)
 		{
 			for (j = i + 1; j < size; j++)
 			{
-				if (temp_1(j,0) == 2 && temp_1(j,1) == temp_1(i,1) && temp_1(j,2) == temp_1(i,2))
+				if (tally_buf(j,0) == 2 && tally_buf(j,1) == tally_buf(i,1) && tally_buf(j,2) == tally_buf(i,2))
 				{
-					temp_1(i,0) = 0; temp_1(i,1) = 0; temp_1(i,2) = 0;
-					temp_1(j,0) = 0; temp_1(j,1) = 0; temp_1(j,2) = 0;
+					tally_buf(i,0) = 0; tally_buf(i,1) = 0; tally_buf(i,2) = 0;
+					tally_buf(j,0) = 0; tally_buf(j,1) = 0; tally_buf(j,2) = 0;
 					cc = cc - 2;
 				}
 			}
@@ -354,11 +364,11 @@ int sort_temp_1_data_sgm(Grid2D& temp_1,int &size,int max_set_size)
 	{
 		for(j=i+1;j<size;j++)
 		{
-			if(temp_1(i,0)<temp_1(j,0))
+			if(tally_buf(i,0)<tally_buf(j,0))
 			{
-				for(k=0;k<max_set_size+3;k++)
+				for(k=0;k<max_options+3;k++)
 				{
-					s=temp_1(i,k);temp_1(i,k)=temp_1(j,k);temp_1(j,k)=s;
+					s=tally_buf(i,k);tally_buf(i,k)=tally_buf(j,k);tally_buf(j,k)=s;
 				}
 			}
 		}
@@ -368,28 +378,28 @@ int sort_temp_1_data_sgm(Grid2D& temp_1,int &size,int max_set_size)
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int find_single_count_symbols(Grid2D& temp_1, Grid2D& temp_2, int max_set_size, int size)
+int find_single_count_symbols(Grid2D& tally_buf, Grid2D& work_buf, int max_options, int size)
 {
 	int i, s, c = 0, flag = 0;
 
-	for (i = 1;i < max_set_size;i++)
+	for (i = 1;i < max_options;i++)
 	{
-		if (temp_1(0,i) > 0)
+		if (tally_buf(0,i) > 0)
 		{
-			s = temp_1(0,i);
-			if (temp_2(s,0) == 1) { c++; }
+			s = tally_buf(0,i);
+			if (work_buf(s,0) == 1) { c++; }
 		}
 
 	}
 
 	if (c > 1)
 	{
-		for (i = 1;i < max_set_size;i++)
+		for (i = 1;i < max_options;i++)
 		{
-			if (temp_1(0,i) > 0)
+			if (tally_buf(0,i) > 0)
 			{
-				s = temp_1(0,i);
-				if (temp_2(s,0) == 1) { temp_2(s,0) = 0; }
+				s = tally_buf(0,i);
+				if (work_buf(s,0) == 1) { work_buf(s,0) = 0; }
 			}
 		}
 		return 1;		
